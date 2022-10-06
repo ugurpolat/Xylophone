4a4c0316c1841a4926056f7ad25fc6a94a0708c2
Ugur Polat <ugurpolat113@gmail.com>
2022-10-06 22:13:35 +0300

    Add sounds

---
+++ Xylophone/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Sound.swift
+//  Xylophone
+//
+//  Created by Ugur Polat on 6.10.2022.
+//
+
+import Foundation
+import AVFoundation
+
+class Player {
+    
+    var player:AVAudioPlayer!
+    
+     func playSound(soundTitle:String){
+        let url = Bundle.main.url(forResource: soundTitle, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+}
--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var player = Player()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        player.playSound(soundTitle: sender.titleLabel!.text!)
+    }
 }
 
